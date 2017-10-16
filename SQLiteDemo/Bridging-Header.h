7cfe14fb12341857211468493d84ee47bf2fbbf5
Pat Murphy <pat@docspera.com>
2017-10-15 23:02:42 -0700

    Initial commit

---
+++ SQLiteDemo/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  SQLiteDemo
+//
+//  Created by Pat Murphy on 10/14/17.
+//  Copyright © 2017 Pat Murphy. All rights reserved.
+//  Need this for Swift to see SQLite and compile
+//
+
+#import <sqlite3.h>
